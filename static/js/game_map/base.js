3818bf83e706fc12deed2dd0dedc6d152d6312aa
AryCra07 <703810491@qq.com>
2022-08-14 13:19:04 +0800

    update blood-strip and timer

--- static/js/game_map/base.js
+++ static/js/game_map/base.js
@@ -15,16 +15,33 @@ class gameMap extends KOFObject {
         this.controller = new Controller(this.$canvas);
 
         this.root.$kof.append($(`<div class="kof-head">
-            <div class="kof-head-hp-0"><div></div<div></div></div>
+            <div class="kof-head-hp-0"><div><div></div></div></div>
                     <div class="kof-head-timer">60</div>
-            <div class="kof-head-hp-1"><div></div<div></div></div>
+            <div class="kof-head-hp-1"><div><div></div></div></div>
         </div>`));
+
+        this.time_left = 60000; // 单位：毫秒
+        this.$timer = this.root.$kof.find(".kof-head-timer");
     }
 
     start() {
     }
 
     update() {
+        this.time_left -= this.timedelta;
+        if (this.time_left < 0) {
+            this.time_left = 0;
+
+            let [a, b] = this.root.players;
+            if (a.status !== 6 && b.status !== 6) {
+                a.status = b.status = 6;
+                a.frame_current_cnt = b.frame_current_cnt = 0;
+                a.vx = b.vx = 0;
+            }
+        }
+
+        this.$timer.text(parseInt(this.time_left / 1000));
+
         this.render();
     }
 
--- static/js/player/base.js
+++ static/js/player/base.js
@@ -31,6 +31,8 @@ class Player extends KOFObject {
         this.frame_current_cnt = 0;
 
         this.hp = 100;
+        this.$hp = this.root.$kof.find(`.kof-head-hp-${this.id}>div`);
+        this.$hp_div = this.$hp.find('div');
     }
 
     start() {
@@ -129,11 +131,19 @@ class Player extends KOFObject {
         this.status = 5;
         this.frame_current_cnt = 0;
 
-        this.hp = Math.max(this.hp - 10, 0);
+        this.hp = Math.max(this.hp - 20, 0);
+
+        this.$hp_div.animate({
+            width: this.$hp.parent().width() * this.hp / 100
+        }, 300);
+        this.$hp.animate({
+            width: this.$hp.parent().width() * this.hp / 100
+        }, 700);
 
         if (this.hp <= 0) {
             this.status = 6;
             this.frame_current_cnt = 0;
+            this.vx = 0;
         }
     }
 
